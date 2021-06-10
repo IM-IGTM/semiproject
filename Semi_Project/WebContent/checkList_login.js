996864e9f87792f3da1c7337313fb3269b4ebe0e
윤현선 <@DESKTOP-B7VFRIP>
2021-06-10 16:00:50 +0900

    로그인 유효성 검사 비밀번호 재확인

--- Semi_Project/WebContent/checkList_login.js
+++ Semi_Project/WebContent/checkList_login.js
@@ -1,20 +1,29 @@
-function check(){
-	
+function check() {
+
 	let regPWInput = document.myForm.regPW;
 	let regPW2Input = document.myForm.regPW2;
 
 
 
-	if (notEquals(regPWInput, regPW2Input)) {
-		alert('비밀번호를 다시 확인하세요');
+
+	if(notEquals(regPWInput, regPW2Input)){
+		alert('비번 재확인 실패');
+		
+		i4Input.value = "";
+		i4Input.focus();
+		
+		return false;
+	}
 
 
-	 return regPWInput.value != regPWInput2.value;
-	}	
 
-	
-	
-	
+
+
+
+
+
+
+
+
 	return true;
-	
 }
